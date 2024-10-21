71cb1033114b079e7bff273c391082b5c8465781
Laxdip Patel <102856079+laxdippatel@users.noreply.github.com>
2024-10-21 23:05:35 +0530

    Add files via upload
    In this file I have done the queries of below in Readme.md .

---
+++ db_operations.sql
@@ -0,0 +1,7 @@
+select count(*) from transactions where transactions.currency='INR\r';
+
+select count(*) from transactions where transactions.currency='INR';
+
+select * from transactions where transactions.currency='USD\r' or transactions.currency='USD';
+
+
